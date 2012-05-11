b03e27351cfc5855ab041d6693525a1dd69da620
Neo Lee <neo.lee@gmail.com>
2012-05-11 10:48:50 +0800

    simple creating post

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'rubygems' if RUBY_VERSION < "1.9"
+require 'sinatra/url_for'
 
 require File.join(File.dirname(__FILE__), 'model.rb')
 
@@ -7,9 +8,28 @@ module IIHT
   class Main < Base
     set :root, File.dirname(__FILE__)
 
+    helpers Sinatra::UrlForHelper
+
     # Routes
     get '/' do
+      @posts = Post.all(:order => [ :created_at.desc ])
       haml :index
+    end
+
+    get '/post/new' do
+      haml :new_post
+    end
+
+    post '/post/' do
+      post = Post.new(
+        :title      => params[:title],
+        :body       => params[:body],
+        :created_at => Time.now
+      )
+      post.user = User.first(:id => 1)
+      post.save
+
+      redirect '/'
     end
   end
 
